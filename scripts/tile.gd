05cb6eb5e881621fe4e95f313012ed19ca5be33b
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-11 13:54:51 +0100

    Refactor  tile function

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -4,37 +4,45 @@ extends TextureButton
 const TileStatus = enums.TileStatus
 
 @onready var parent: Board = get_parent()
+@onready var coordinates: Vector2i = get_coordinates()
 var status := TileStatus.EMPTY
 
-#TODO cleanup this function
 func _on_pressed() -> void:
 	if status != TileStatus.EMPTY:
 		return
-	var this_tile = get_coordinates()
+	var to_remove = get_to_remove()
+	if is_valid_move(to_remove):
+		set_status_and_texture()
+		parent.move_played(to_remove)
+
+func get_to_remove() -> Array[Vector2i]:
 	var board := parent.generate_board()
 	if parent.is_black_playing:
-		board[this_tile.x + 1][this_tile.y + 1] = TileStatus.BLACK
+		board[coordinates.x + 1][coordinates.y + 1] = TileStatus.BLACK
 	else:
-		board[this_tile.x + 1][this_tile.y + 1] = TileStatus.WHITE
+		board[coordinates.x + 1][coordinates.y + 1] = TileStatus.WHITE
 	var check_captures := CheckCaptures.new(parent.grid_size)
-	var to_remove := check_captures.analyse_board(board)
-	if this_tile in to_remove:
-		return
+	return check_captures.analyse_board(board)
+
+func is_valid_move(to_remove: Array[Vector2i]) -> bool:
+	if coordinates in to_remove:
+		return false
+	return true
+
+func set_status_and_texture() -> void:
 	if parent.is_black_playing:
 		self.texture_normal = load("res://assets/images/black.svg")
 		status = TileStatus.BLACK
-		parent.move_played(to_remove)
 	else:
 		self.texture_normal = load("res://assets/images/white.svg")
 		status = TileStatus.WHITE
-		parent.move_played(to_remove)
+
+func make_empty() -> void:
+	status = TileStatus.EMPTY
+	self.texture_normal = load("res://assets/images/empty.svg")
 
 func get_coordinates() -> Vector2i:
 	var index := get_index()
 	var x := index % parent.grid_size
 	var y := index / parent.grid_size
 	return Vector2i(x, y)
-
-func make_empty() -> void:
-	status = TileStatus.EMPTY
-	self.texture_normal = load("res://assets/images/empty.svg")
