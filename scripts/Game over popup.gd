9e70780168b3a12e5fba382757edcf9fbc4bc2cf
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-21 19:00:55 +0100

    Cleanup and make popup prettier

--- scripts/Game over popup.gd
+++ scripts/Game over popup.gd
@@ -1,8 +1,17 @@
 extends Popup
 
 func display_popup(text):
-	$ Label.text = text
+	var label: Label = $ Contents/Winner
+	label.text = text
 	popup()
 
 func _on_popup_hide():
+	back_to_main_menu()
+
+func _on_ok_pressed():
+	back_to_main_menu()
+
+func back_to_main_menu():
+	Globals.is_black_playing = true
+	Globals.player_just_passed = false
 	get_parent().queue_free()
