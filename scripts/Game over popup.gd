eca54268e807f7f56bb5fe480ecb06af48279a13
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-22 09:02:32 +0100

    Replace Popup window with standard prettier nodes

--- scripts/Game over popup.gd
+++ scripts/Game over popup.gd
@@ -1,15 +1,9 @@
-extends Popup
+extends ColorRect
 
 func display_popup(text):
-	var label: Label = $ Contents/Winner
+	var label: Label = $ Tile/Contents/Winner
 	label.text = text
-	popup()
-
-func _on_popup_hide():
-	back_to_main_menu()
-
-func _on_ok_pressed():
-	back_to_main_menu()
+	show()
 
 func back_to_main_menu():
 	Globals.is_black_playing = true
